3e30a68470d84b0d08c96ce02445cb82adc57ae1
alokgogate <alokgogate@gmail.com>
2017-05-07 20:14:37 +0100

    Update R-part with further model
    New models with confusion matrix

--- Code/R-Part/Rpart.R
+++ Code/R-Part/Rpart.R
@@ -9,12 +9,21 @@ test <-sample_frac(data,0.2)
 
 
 #R-Part
+  #make churn a factor to predict 
+  test$churn <-as.factor(test$churn)
+  #R-Part using method="class"
+  rpart_tree <-rpart(formula = churn~.,data=train,method='class')
+  summary(rpart_tree)
+  plot(rpart_tree)
+  text(rpart_tree)
 
-rpart_tree <-rpart(formula = churn~.,data=train,method='class')
-summary(rpart_tree)
-plot(rpart_tree)
-text(rpart_tree)
-
-pred <-predict(rpart_tree,newdata = test)
-
-comp <-cbind(pred,test$churn)
+  pred <-predict(rpart_tree,newdata = test,type = "class")
+  conf<-confusionMatrix(pred,test$churn)
+  #comp <-cbind(pred,test$churn)
+  #R-part with twicking some parameters
+  
+  rpart_tree_1 <-rpart(churn~.,data=train,method="class",control = rpart.control(minsplit = 2,cp=0))
+  pred_1 <-predict(rpart_tree_1,newdata = test,type="class")
+  #comp_1 <-cbind(pred_1,test$churn)
+  
+  conf_1<-confusionMatrix(pred_1,test$churn)
