6dd2c0e7905f9fa72727ab3a84bc063eb7a9bd0a
Abhishek Sharma <abs_sh@hotmail.co.uk>
2017-05-08 02:57:18 +0100

    Update nn script and workspace

--- Code/NeuralNet/neuralNet.R
+++ Code/NeuralNet/neuralNet.R
@@ -15,8 +15,10 @@ library(plyr)   # progress bar library
 #               READ DATA AND CHECK FOR MISSING VALUES
 # -----------------------------------------------------------------
 # read 100k data - drop customer ID
-churn_100k <- final_data[complete.cases(final_data),]
-churn_100k <- churn_100k[, !names(churn_100k) %in% c("Customer_ID")]
+final_data <- read.csv("final_csv.csv")
+churn_100k <- final_data
+churn_100k <- churn_100k[complete.cases(churn_100k),]
+churn_100k <- churn_100k[, !names(churn_100k) %in% c("X","Customer_ID")]
 
 # create subset of 1k for test purpose
 churn_1k <- subset(churn_100k[1:1000,])
@@ -26,7 +28,7 @@ any(is.na(churn_1k))
 any(is.na(churn_100k))
 
 # -----------------------------------------------------------------
-#         SCALE DATA, CREATE TEST/TRAIN SETS
+#             SCALE DATA, CREATE TEST/TRAIN SETS
 # -----------------------------------------------------------------
 # read variable names
 allVars <- colnames(churn_1k)
@@ -48,7 +50,7 @@ train_data <- scaled[index,]
 test_data <- scaled[-index,]
 
 # -----------------------------------------------------------------
-#           CREATE REG AND RF MODELS FOR COMPARISON
+#                CREATE LOG R MODEL FOR COMPARISON
 # -----------------------------------------------------------------
 # logistic regression
 glm.fit = glm(form, data = train_data)
@@ -58,29 +60,27 @@ glm.mse = sum((glm.pred - test_data$churn)^2)/nrow(test_data)
 glm.mse
 
 # random forest to determine important variables
-# won't work for 100k unless missing rows found earlier
-# are removed or filled in
+# TODO: ADD RANDOM FOREST MODEL HERE TO USE AS COMPARISON
 randomForest = randomForest(churn ~., data = churn_1k, importance = TRUE)
 varImpPlot(randomForest, main = "Random Forest Variable Importance Plot")
+importance(randomForest)
 
 # -----------------------------------------------------------------
-#                 NEURAL NETWORK MODEL
+#                     NEURAL NETWORK MODEL
 # -----------------------------------------------------------------
 # build and plot neural net model
 # config = predictor-hidden(2nd)-hidden(3rd)-output e.g. 13-4-2-1
 set.seed(333)  #set seed to get same result every time
 nn <- neuralnet(formula = form, hidden = 2, linear.output = F, 
-                       data = train_data, err.fct = "sse", 
-                       act.fct = "logistic", algorithm = "rprop+")
+                data = train_data, err.fct = "sse", 
+                act.fct = "logistic", algorithm = "rprop+", stepmax = 1e+06)
 plot(nn)
 
 nn$weights     #weights for neural network model
 nn$net.result  #output for each replication
 nn$result.matrix #neural net result matrix
 nn$covariate    #neural net covariates
-# -----------------------------------------------------------------
-#                   NEURAL NETWORK PREDICTIONS
-# -----------------------------------------------------------------
+
 # do predictions
 nn.predictions <- compute(nn, test_data[, !names(test_data) %in% "churn"])
 nn.predictions$net.result
@@ -106,10 +106,10 @@ plot(test_data$churn, nn.predictions, col = 'red', main = c("Neural Network Mode
 abline(0,1, col = "black")
 
 # -----------------------------------------------------------------
-# PERFORMANCE PLOTS + COMPARISON WITH GLM
+#               PERFORMANCE PLOTS + COMPARISON WITH GLM
 # -----------------------------------------------------------------
 # side by side comparison of NN and GLM
-par(mfrow=c(2,1))
+par(mfrow=c(1,2))
 
 plot(test.actual,nn.predictions,col='red',main='Real vs Predicted NN', xlab = "Actual", ylab = "Predicted", pch=18,cex=0.7)
 abline(0,1,lwd=2)
@@ -127,7 +127,7 @@ abline(0,1,lwd=2)
 legend('bottomright',legend=c('NN','GLM'),pch=18,col=c('red','blue'))
 
 # -----------------------------------------------------------------
-# CROSS VALIDATION
+#                       CROSS VALIDATION
 # -----------------------------------------------------------------
 # cross validation for glm
 set.seed(666)
@@ -142,20 +142,19 @@ k <- 10
 
 pbar <- create_progress_bar('text')
 pbar$init(k)
-
 for(i in 1:k){
   index <- sample(1:nrow(churn_1k),round(0.8*nrow(churn_1k)))
   train_data <- scaled[index,]
   test_data <- scaled[-index,]
   
-  nn <- neuralnet(form,data=train_data,hidden=2,linear.output=F)
+  nn <- neuralnet(form,data=train_data,hidden=2,linear.output=F, stepmax = 1e+06)
   
   nn.predictions <- compute(nn, test_data[, !names(test_data) %in% "churn"])
   nn.predictions <- nn.predictions$net.result*(max(churn_1k$churn)-min(churn_1k$churn))+min(churn_1k$churn)
   
-  test.actual <- (test.cv$churn)*(max(churn_1k$churn)-min(churn_1k$churn))+min(churn_1k$churn)
+  test.actual <- (test_data$churn)*(max(churn_1k$churn)-min(churn_1k$churn))+min(churn_1k$churn)
   
-  nn.cv[i] <- sum((test.actual - nn.predictions)^2)/nrow(test.cv)
+  nn.cv[i] <- sum((test.actual - nn.predictions)^2)/nrow(test_data)
   
   pbar$step()
 }
